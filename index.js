30a957ae253677e6a5ab65e8a427c4b80f5af77c
Klaus Hartl <klaus.hartl@gmail.com>
2022-05-10 13:47:55 +0200

    Split tests + fix result when passing
    Bug uncovered by more fine-grained tests.

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ export default (pattern) =>
     .filter((step) => step.hasOwnProperty("uses"))
     .map((step) => step.uses)
     .filter((uses) => !uses.startsWith("actions/"))
-    .map((uses) => {
+    .reduce((accumulator, uses) => {
       if (
         uses.split("@").length !== 2 ||
         uses.split("@")[1].search(/[a-z0-9]{40}$/) === -1
       ) {
-        return `${uses} should use a commit hash as a version identifier`
+        accumulator.push(
+          `${uses} should use a commit hash as a version identifier`
+        )
       }
-    })
+      return accumulator
+    }, [])
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,14 @@
 import linter from "../index"
 
-test("Calling linter", () => {
-  expect(linter("./test/fixtures/**/*.yml")).toContain(
-    "crazy-max/ghaction-github-pages@v2 should use a commit hash as a version identifier",
-    "crazy-max/ghaction-github-pages@latest should use a commit hash as a version identifier"
-  )
+test("Inacceptable branch reference", () => {
+  expect(linter("./test/fixtures/branch.yml").length).toBe(1)
+})
+
+test("Inacceptable tag reference", () => {
+  expect(linter("./test/fixtures/tag.yml").length).toBe(1)
+})
+
+test("Passing workflow", () => {
+  console.log(linter("./test/fixtures/passing.yml"))
+  // expect(linter("./test/fixtures/passing.yml").length).toBe(0)
 })
