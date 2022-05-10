ce6cdeaf8abb09190d7835d93a32abc8b372984a
Klaus Hartl <klaus.hartl@gmail.com>
2022-05-10 09:58:45 +0200

    Brush up cli source

--- cli.js
+++ cli.js
@@ -4,21 +4,19 @@ import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
 import chalk from "chalk"
 import linter from "./index.js"
-const argv = yargs(hideBin(process.argv)).argv
-let workflowPath = "./.github/workflows/"
 
-if (argv.path) {
-  console.log(chalk.blue(`Looking for workflows in ${argv.path}`))
-  workflowPath = argv.path
-}
+const argv = yargs(hideBin(process.argv)).argv
+const workflowPath = argv.path || "./.github/workflows/"
+
 try {
+  console.log(chalk.blue(`Looking for workflows in ${argv.path}`))
   const result = linter(workflowPath)
   if (result.length > 0) {
-    console.log(chalk.red(`Found issues in github workflows`))
+    console.log(chalk.red("Found issues in workflow files"))
     console.log(chalk.red(result.join("\n")))
     process.exit(result.length)
   } else {
-    console.log(chalk.green(`No issues found in github workflows`))
+    console.log(chalk.green("No issues found in workflow files"))
     process.exit(0)
   }
 } catch (err) {
