fa9d8004403082b79e4438884c4d23ce890da748
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2024-10-28 10:58:02 +0100

    prettier fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,32 @@
 import { includeIgnoreFile } from "@eslint/compat"
 import js from "@eslint/js"
-import globals from "globals";
+import globals from "globals"
 import { fileURLToPath, URL } from "node:url"
-import jest from "eslint-plugin-jest";
+import jest from "eslint-plugin-jest"
 
 export default [
-    includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
+  includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
 
-    // Basic rules
-    js.configs.recommended,
+  // Basic rules
+  js.configs.recommended,
 
-    {
-        files: ["**/*.js"],
+  {
+    files: ["**/*.js"],
 
-        languageOptions: {
-            globals: { ...globals.node },
-            ecmaVersion: 2022,
-            sourceType: "module",
-        },
+    languageOptions: {
+      globals: { ...globals.node },
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
-    {
-        languageOptions: {
-            globals: { ...globals.jest }
-        },
-        files: ['test/*.test.js'],
-        ...jest.configs['flat/recommended'],
-        rules: {
-            ...jest.configs['flat/recommended'].rules
-        }
-    }
-];
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.jest },
+    },
+    files: ["test/*.test.js"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+    },
+  },
+]
