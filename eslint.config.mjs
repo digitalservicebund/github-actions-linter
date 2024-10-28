3f4003a20e362b97f5e7ad6de29e85ae9b5f8fce
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2024-10-28 10:53:55 +0100

    migrate eslint config to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { includeIgnoreFile } from "@eslint/compat"
+import js from "@eslint/js"
+import globals from "globals";
+import { fileURLToPath, URL } from "node:url"
+import jest from "eslint-plugin-jest";
+
+export default [
+    includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
+
+    // Basic rules
+    js.configs.recommended,
+
+    {
+        files: ["**/*.js"],
+
+        languageOptions: {
+            globals: { ...globals.node },
+            ecmaVersion: 2022,
+            sourceType: "module",
+        },
+    },
+    {
+        languageOptions: {
+            globals: { ...globals.jest }
+        },
+        files: ['test/*.test.js'],
+        ...jest.configs['flat/recommended'],
+        rules: {
+            ...jest.configs['flat/recommended'].rules
+        }
+    }
+];
